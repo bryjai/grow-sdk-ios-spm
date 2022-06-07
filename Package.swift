b816d9b6f4e6846fba58c8e5132db3d509db44ea
Filipe Martins <>
2022-06-07 11:25:10 +0100

    fix checksum for release 1.0.2

--- Package.swift
+++ Package.swift
@@ -16,10 +16,6 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "GrowSDK", url: "https://s3-eu-west-1.amazonaws.com/bryj-sdks/ios/1.0.2/Grow-SDK-iOS-1.0.2.xcframework.zip", checksum: "2022-05-25 16:09:43.419 xcodebuild[4160:27476] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionSentinelHostApplications for extension Xcode.DebuggerFoundation.AppExtensionHosts.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
-2022-05-25 16:09:43.419 xcodebuild[4160:27476] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionPointIdentifierToBundleIdentifier for extension Xcode.DebuggerFoundation.AppExtensionToBundleIdentifierMap.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
-2022-05-25 16:09:44.495 xcodebuild[4161:27487] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionSentinelHostApplications for extension Xcode.DebuggerFoundation.AppExtensionHosts.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
-2022-05-25 16:09:44.495 xcodebuild[4161:27487] Requested but did not find extension point with identifier Xcode.IDEKit.ExtensionPointIdentifierToBundleIdentifier for extension Xcode.DebuggerFoundation.AppExtensionToBundleIdentifierMap.watchOS of plug-in com.apple.dt.IDEWatchSupportCore
-146b2b513f8c540df9413ff43fa1730dabe242b8c0636d3f0812d67dfcc8c34e")
+        .binaryTarget(name: "GrowSDK", url: "https://s3-eu-west-1.amazonaws.com/bryj-sdks/ios/1.0.2/Grow-SDK-iOS-1.0.2.xcframework.zip", checksum: "146b2b513f8c540df9413ff43fa1730dabe242b8c0636d3f0812d67dfcc8c34e")
     ]
 )

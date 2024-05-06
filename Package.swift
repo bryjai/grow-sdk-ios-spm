74b623ddddfdedc831e44330e08682ee24e4e13f
shahabfollow <shahab@followanalytics.com>
2024-05-06 14:50:16 -0400

    release 1.2.3

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "GrowSDK", url: "https://s3-eu-west-1.amazonaws.com/bryj-sdks/ios/1.2.3/Grow-SDK-iOS-1.2.3.xcframework.zip", checksum: "c923cf764f4f689848331af8690de4db83849f00fff149f69731f9863d7cc913")
+        .binaryTarget(name: "GrowSDK", url: "https://s3-eu-west-1.amazonaws.com/bryj-sdks/ios/1.2.3/Grow-SDK-iOS-1.2.3.xcframework.zip", checksum: "c8875d73ee071c3aae359c8c91782b8eb5e8c0b5610c0a930a0e5c50ad8f3e55")
     ]
 )
